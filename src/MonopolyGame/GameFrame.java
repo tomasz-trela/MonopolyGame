475e7c0013d20af6acd4dad93d1d4f2a91225659
TTomaszPWR <280598@student.pwr.edu.pl>
2023-12-12 18:42:33 +0100

    Interface Window

---
+++ src/MonopolyGame/GameFrame.java
@@ -0,0 +1,17 @@
+package MonopolyGame;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    GameFrame(){
+        this.add(new GamePanel());
+        this.setTitle("Monopoly");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setLayout(null);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
+}
---
+++ src/MonopolyGame/MonopolyGame.java
@@ -0,0 +1,8 @@
+package MonopolyGame;
+
+public class MonopolyGame {
+    public static void main(String[] args) {
+        new GameFrame();
+
+        }
+    }
