900500c6cc5e6e1a033c500ec3e96db4621d534f
TTomaszPWR <280598@student.pwr.edu.pl>
2023-12-12 17:36:12 +0100

    test
    test

--- src/MonopolyGame/Main.java
+++ src/MonopolyGame/Main.java
@@ -5,6 +5,6 @@ package MonopolyGame;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Hello and welcome!");
+        System.out.print("Welcome and Hello!");
         }
     }
