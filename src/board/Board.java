b02f9fc241627d41e7c4b7057724ec33e206e925
TTomaszPWR <280598@student.pwr.edu.pl>
2023-12-13 11:44:48 +0100

    quick update
    Lepsza wersja + wiecej komentarzy

--- src/board/Board.java
+++ src/board/Board.java
@@ -3,23 +3,6 @@ package board;
 import java.awt.*;
 
 public class Board {
-    private final static int BOARD_WIDTH;
-    private final static int BOARD_HEIGHT;
-    private final static int FIELD_WIDTH;
-    private final static int FIELD_HEIGHT;
-    private final static Color BOARD_COLOR;
-    private final static Color FIELD_COLOR1;
-    private final static Color FIELD_COLOR2;
-    static {
-        BOARD_WIDTH = 528;
-        BOARD_HEIGHT = 528;
-        BOARD_COLOR = new Color(200, 224,196);
-        FIELD_COLOR1 = new Color(201, 200,252);
-        FIELD_COLOR2 = new Color(252, 221,201);
-        FIELD_WIDTH = BOARD_WIDTH/12-2;
-        FIELD_HEIGHT = FIELD_WIDTH*2+1;
-    }
-
     private Field[] fields = new Field[36];
     private int move_counter; //licznik ruchów
     private int round; //ilość okrążeń gracza z największą ich iloscią
@@ -32,34 +15,7 @@ public class Board {
             fields[i] = new Field("Pole %s".formatted(i));
         }
     }
-
-    public int getBoardWidth()
-    {
-        return BOARD_WIDTH;
-    }
-    public int getBoardHeigtht()
-    {
-        return BOARD_HEIGHT;
-    }
-    public Color getBoardColor()
-    {
-        return BOARD_COLOR;
-    }
     public Field[] getFieldsArray(){
         return fields;
-    }
-    public static int getFieldHeight() {
-        return FIELD_HEIGHT;
-    }
-    public static int getFieldWidth() {
-        return FIELD_WIDTH;
-    }
-
-    public static Color getFieldColor1() {
-        return FIELD_COLOR1;
-    }
-
-    public static Color getFieldColor2() {
-        return FIELD_COLOR2;
     }
 }
