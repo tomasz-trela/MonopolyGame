4555f58b965cd844b74f07bf7cab413bed11a167
DawidWyskwarski <280500@student.pwr.edu.pl>
2023-12-15 16:46:51 +0100

    dodanie klasy gracz
    implementacja ruchu po planszy jak coś to poprawcie jeśli cos bez sensu napisałem

--- src/monopolyGame/MonopolyGame.java
+++ src/monopolyGame/MonopolyGame.java
@@ -1,8 +1,27 @@
 package monopolyGame;
+import player.*;
 
 public class MonopolyGame {
     public static void main(String[] args) {
+     
         new GameFrame();
 
+
+        // Na dole jest implementacja ruchu gracza w konsoli
+        // teraz to jeszcze graficznie trza
+        
+        /*Player gracz1 = new Player();
+
+        while(gracz1.getOkrazenie() != 2){
+            int tmp = gracz1.getPole();
+            System.out.println("Gracz jest na: " + gracz1.getPole() + " okrazenie " + gracz1.getOkrazenie());
+            
+            gracz1.setPole(gracz1.getPole() + gracz1.rzutKostka() + gracz1.rzutKostka() );
+            if(tmp>gracz1.getPole())
+                gracz1.setOkrazenie(gracz1.getOkrazenie()+1);
         }
+        
+        System.out.println("Gracz jest na: " + gracz1.getPole() + " okrazenie " + gracz1.getOkrazenie());
+        */
     }
+}
--- src/player/Player.java
+++ src/player/Player.java
@@ -1,4 +1,67 @@
 package player;
 
+import java.util.ArrayList;
+
 public class Player {
+    private int okrazenie;
+    private double saldoEuro;
+    private double saldoDolar;
+    private boolean czyAuto;
+    private ArrayList<Integer> PosiadanePola = new ArrayList<Integer>();
+    private int pole; 
+
+    public Player(){
+        this.saldoEuro = 50000;
+        this.saldoDolar = 50000;
+        this.czyAuto = false;
+        this.PosiadanePola.clear(); 
+        this.pole = 0;
+        this.okrazenie = 0;
+    }
+
+    //gettery settery
+    public double getSaldoEuro() {
+        return saldoEuro;
+    }
+
+    public void setSaldoEuro(double saldoEuro) {
+        this.saldoEuro = saldoEuro;
+    }
+
+    public double getSaldoDolar() {
+        return saldoDolar;
+    }
+
+    public void setSaldoDolar(double saldoDolar) {
+        this.saldoDolar = saldoDolar;
+    }
+
+    public boolean GetCzyAuto() {
+        return czyAuto;
+    }
+
+    public void setCzyAuto(boolean czyAuto) {
+        this.czyAuto = czyAuto;
+    }
+
+    public int getPole() {
+        return pole;
+    }
+
+    public void setPole(int pole) {
+        this.pole = (pole%36);
+    }
+
+    public int getOkrazenie() {
+        return okrazenie;
+    }
+
+    public void setOkrazenie(int okrazenie) {
+        this.okrazenie = okrazenie;
+    }
+
+    //nie lepiej zamiast całej klasy kostka tutaj to zrobic ? (jak coś to to zmieć)
+    public int rzutKostka(){
+        return (int) ((Math.random()*6) + 1); 
+    }
 }
