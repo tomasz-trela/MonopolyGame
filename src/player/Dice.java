d708a28689fc5468669b8dc7ccd98ce30cb2878a
DawidWyskwarski <280500@student.pwr.edu.pl>
2023-12-16 20:03:55 +0100

    poprawka

--- src/player/Dice.java
+++ src/player/Dice.java
@@ -3,12 +3,10 @@ package player;
 import java.util.Random;
 
 public class Dice {
+    
     public static int Roll(){
         Random random = new Random();
         int value1 = random.nextInt(6)+1;
         return value1;
     }
-
-
-
 }
