fde9096cafa8fcd7b21a1ddbca472f0a8b5b574c
Drake3001 <96744056+Drake3001@users.noreply.github.com>
2023-12-21 20:21:26 +0100

    Aktualizacja planszy

--- src/monopolyGame/GamePanel.java
+++ src/monopolyGame/GamePanel.java
@@ -1,14 +1,9 @@
 package monopolyGame;
 
 import java.awt.Font;
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.*;
-import java.net.URL;
-import java.util.Scanner;
-
 import board.*;
 import static player.Dice.Roll;
 import player.*;
@@ -18,17 +13,20 @@ public class GamePanel extends JPanel implements ActionListener {
     private static final int SCREEN_HEIGHT;
     private final static int BOARD_WIDTH;
     private final static int BOARD_HEIGHT;
-    private final static int FIELD_WIDTH;
-    private final static int FIELD_HEIGHT;
+    private static int FIELD_WIDTH;
+    private static int FIELD_HEIGHT;
     private final static int ROLL_BUTTON_HEIGHT;
     private final static int ROLL_BUTTON_WIDTH;
     private final static Color BOARD_COLOR;
     private final static Color FIELD_COLOR1;
     private final static Color FIELD_COLOR2;
 
+
+    private static JPanel[] fieldArray;
+
     static {
-        SCREEN_WIDTH=1100;
-        SCREEN_HEIGHT = 750;
+        SCREEN_WIDTH=1500;
+        SCREEN_HEIGHT = 1000;
         BOARD_WIDTH = 528;
         BOARD_HEIGHT = 528;
         BOARD_COLOR = new Color(200, 224,196);
@@ -43,8 +41,9 @@ public class GamePanel extends JPanel implements ActionListener {
     JButton rollButton;
     JLabel diceLabel1;
     JLabel diceLabel2;
+    JPanel leftPanel=new JPanel();
+    JPanel rightPanel= new JPanel();
 
-    private static int poz[][]=new int[36][4];
     private boolean start=true;
     private int round;
     Pawn pawn0=new Pawn(0);
@@ -52,134 +51,134 @@ public class GamePanel extends JPanel implements ActionListener {
     Pawn pawn2=new Pawn(2);
     Pawn pawn3=new Pawn(3);
     GamePanel(){
+
         this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
         this.setBackground(Color.WHITE);
+        this.setLayout(null);
+        leftPanel.setBounds(0,0,1000, 1000);
+        rightPanel.setBounds(1000,0,500, 1000);
+        leftPanel.setLayout(null);
+        rightPanel.setLayout(null);
+        leftPanel.setVisible(true);
+        rightPanel.setVisible(true);
+        this.add(leftPanel);
+        this.add(rightPanel);
         this.setFocusable(true);
         createRollButton();
         createDiceLabels();
-        this.setLayout(null);
-        this.add(rollButton);
+        leftPanel.add(rollButton);
         rollButton.setVisible(true);
-        this.add(diceLabel1);
-        this.add(diceLabel2);
-        this.add(pawn1.getPawn());
-        this.add(pawn2.getPawn());
-        this.add(pawn3.getPawn());
-        this.add(pawn0.getPawn());
-        this.setVisible(true);
-    }
 
+        createBoard(36);
+
+        leftPanel.add(diceLabel1);
+        leftPanel.add(diceLabel2);
+        leftPanel.add(pawn1.getPawn());
+        leftPanel.add(pawn2.getPawn());
+        leftPanel.add(pawn3.getPawn());
+        leftPanel.add(pawn0.getPawn());
+        this.setVisible(true);
+        this.setPawnsStart();
+    }
     public void setPawnsStart(){
         pawn0.placePawnOn(0);
         pawn1.placePawnOn(0);
         pawn2.placePawnOn(0);
         pawn3.placePawnOn(0);
     }
-    public void paintComponent(Graphics g){
-        super.paintComponent(g);
-        draw(g);
-    }
-     public void draw(Graphics g) {
-        ImageIcon imageIcon = new ImageIcon("src/images/menuBackground.jpg");
-        Image backgroundImage = imageIcon.getImage();
 
-        // Draw the image without automatic scaling
-         g.drawImage(backgroundImage, 0, 0, 1100, 1100, this);
+    public void createBoard(int fieldnumber){
+        fieldArray=new JPanel[fieldnumber];
+
+        FIELD_WIDTH=800/((fieldnumber/4)+3);
+        FIELD_HEIGHT=2*FIELD_WIDTH;
+        pawn0.SizeSet(FIELD_WIDTH/2, FIELD_WIDTH/2);
+        pawn1.SizeSet(FIELD_WIDTH/2, FIELD_WIDTH/2);
+        pawn2.SizeSet(FIELD_WIDTH/2, FIELD_WIDTH/2);
+        pawn3.SizeSet(FIELD_WIDTH/2, FIELD_WIDTH/2);
 
 
-        g.setColor(BOARD_COLOR);//kolor Planszy
-        g.fillRect((this.getWidth() - BOARD_WIDTH) / 2, (this.getHeight() - BOARD_HEIGHT) / 2, BOARD_WIDTH, BOARD_HEIGHT);//rysujemy prostokąt na środku to jest plansza
-
-        g.setColor(Color.BLACK);//kolor Marginesów
-        g.fillRect((this.getWidth() - BOARD_WIDTH) / 2, (this.getHeight() - BOARD_HEIGHT) / 2, BOARD_WIDTH, FIELD_HEIGHT + 4);
-        g.fillRect((this.getWidth() - BOARD_WIDTH) / 2, (this.getHeight() - BOARD_HEIGHT) / 2 + (BOARD_HEIGHT - (FIELD_HEIGHT + 4)), BOARD_WIDTH, FIELD_HEIGHT + 4);
-        g.fillRect((this.getWidth() - BOARD_WIDTH) / 2, (this.getHeight() - BOARD_HEIGHT) / 2, FIELD_HEIGHT + 4, BOARD_HEIGHT);
-        g.fillRect((this.getWidth() - BOARD_WIDTH) / 2 + (BOARD_WIDTH - (FIELD_HEIGHT + 4)), (this.getHeight() - BOARD_HEIGHT) / 2, FIELD_HEIGHT + 4, BOARD_HEIGHT);
-        //rysujemy czarne prostokąty na których powstaną kafelki dzięki czemu zostaną nam marginesy między kafelkami
-        int tmp = 0;//zmienna pomocnicza do rysowania kafelków w pentli
-        for (int i = 0; i < board.getFieldsArray().length; i++) {
-
-            if (i == 0) {
-                g.setColor(FIELD_COLOR2);//Kolor Narożników
-                g.fillRect((this.getWidth() - BOARD_WIDTH) / 2 + 2, (this.getHeight() - BOARD_HEIGHT) / 2 + (BOARD_HEIGHT - (FIELD_HEIGHT + 2)), FIELD_HEIGHT, FIELD_HEIGHT);
-                tmp += FIELD_WIDTH + 2;
-                poz[i][0]=((this.getWidth() - BOARD_WIDTH) / 2 + 2);
-                poz[i][1]=(this.getHeight() - BOARD_HEIGHT) / 2 + (BOARD_HEIGHT - (FIELD_HEIGHT + 2));
-                poz[i][2]=poz[i][0]+FIELD_HEIGHT;
-                poz[i][3]=poz[i][1]+FIELD_HEIGHT;
-            }//rysujemy narożnik
-            else if (i < 9) {
-                 g.setColor(FIELD_COLOR1);//Kolor Pól do kupienia
-                g.fillRect((this.getWidth() - BOARD_WIDTH) / 2 + 2, (this.getHeight() - BOARD_HEIGHT) / 2 + (BOARD_HEIGHT - (FIELD_HEIGHT + 2)) - tmp, FIELD_HEIGHT, FIELD_WIDTH);
-                poz[i][0]=(this.getWidth() - BOARD_WIDTH) / 2 + 2;
-                poz[i][1]=(this.getHeight() - BOARD_HEIGHT) / 2 + (BOARD_HEIGHT - (FIELD_HEIGHT + 2)) - tmp;
-                poz[i][2]=poz[i][0]+FIELD_HEIGHT;
-                poz[i][3]=poz[i][1]+FIELD_WIDTH;
-                tmp += FIELD_WIDTH + 2;
-            }//rysujemy kafelki pomiedzy narożnikami
-            else if (i == 9) {
-                tmp = 0;
-                g.setColor(FIELD_COLOR2);//Kolor Narożników
-                g.fillRect((this.getWidth() - BOARD_WIDTH) / 2 + 2, (this.getHeight() - BOARD_HEIGHT) / 2 + 2, FIELD_HEIGHT, FIELD_HEIGHT);
-                poz[i][0]=(this.getWidth() - BOARD_WIDTH) / 2 + 2;
-                poz[i][1]=(this.getHeight() - BOARD_HEIGHT) / 2 + 2;
-                poz[i][2]=poz[i][0]+FIELD_HEIGHT;
-                poz[i][3]=poz[i][1]+FIELD_HEIGHT;
-                tmp += FIELD_HEIGHT + 2;
-            }//rysujemy narożnik
-            else if (i < 18) {
-                g.setColor(FIELD_COLOR1);//Kolor Pól do kupienia
-                g.fillRect((this.getWidth() - BOARD_WIDTH) / 2 + 2 + tmp, (this.getHeight() - BOARD_HEIGHT) / 2 + 2, FIELD_WIDTH, FIELD_HEIGHT);
-                poz[i][0]=(this.getWidth() - BOARD_WIDTH) / 2 + 2 + tmp;
-                poz[i][1]=(this.getHeight() - BOARD_HEIGHT) / 2 + 2;
-                poz[i][2]=poz[i][0]+FIELD_WIDTH;
-                poz[i][3]=poz[i][1]+FIELD_HEIGHT;
-                tmp += FIELD_WIDTH + 2;
-            }//rysujemy kafelki pomiedzy narożnikami
-            else if (i == 18) {
-                tmp = 0;
-                g.setColor(FIELD_COLOR2);//Kolor Narożników
-                g.fillRect((this.getWidth() - BOARD_WIDTH) / 2 + (BOARD_WIDTH - (FIELD_HEIGHT + 2)), (this.getHeight() - BOARD_HEIGHT) / 2 + 2, FIELD_HEIGHT, FIELD_HEIGHT);
-                poz[i][0]=(this.getWidth() - BOARD_WIDTH) / 2 + (BOARD_WIDTH - (FIELD_HEIGHT + 2));
-                poz[i][1]=(this.getHeight() - BOARD_HEIGHT) / 2 + 2;
-                poz[i][2]=poz[i][0]+ FIELD_HEIGHT;
-                poz[i][3]=poz[i][1]+FIELD_HEIGHT;
-                tmp += FIELD_HEIGHT + 2;
-            }//rysujemy narożnik
-            else if (i < 27) {
-                g.setColor(FIELD_COLOR1);//Kolor Pól do kupienia
-                g.fillRect((this.getWidth() - BOARD_WIDTH) / 2 + (BOARD_WIDTH - (FIELD_HEIGHT + 2)), (this.getHeight() - BOARD_HEIGHT) / 2 + 2 + tmp, FIELD_HEIGHT, FIELD_WIDTH);
-                poz[i][0]=(this.getWidth() - BOARD_WIDTH) / 2 + (BOARD_WIDTH - (FIELD_HEIGHT + 2));
-                poz[i][1]=(this.getHeight() - BOARD_HEIGHT) / 2 + 2 + tmp;
-                poz[i][2]=poz[i][0]+FIELD_HEIGHT;
-                poz[i][3]=poz[i][1]+FIELD_WIDTH;
-                tmp += FIELD_WIDTH + 2;
-            }//rysujemy kafelki pomiedzy narożnikami
-            else if (i == 27) {
-                tmp = 0;
-                g.setColor(FIELD_COLOR2);//Kolor Narożników
-                g.fillRect((this.getWidth() - BOARD_WIDTH) / 2 + (BOARD_WIDTH - (FIELD_HEIGHT + 2)), (this.getHeight() - BOARD_HEIGHT) / 2 + (BOARD_HEIGHT - (FIELD_HEIGHT + 2)), FIELD_HEIGHT, FIELD_HEIGHT);
-                poz[i][0]=(this.getWidth() - BOARD_WIDTH) / 2 + (BOARD_WIDTH - (FIELD_HEIGHT + 2));
-                poz[i][1]=(this.getHeight() - BOARD_HEIGHT) / 2 + (BOARD_HEIGHT - (FIELD_HEIGHT + 2));
-                poz[i][2]=poz[i][0]+FIELD_HEIGHT;
-                poz[i][3]=poz[i][1]+FIELD_HEIGHT;
-                tmp += FIELD_HEIGHT + 2;
-            }//rysujemy narożnik
-            else {
-                g.setColor(FIELD_COLOR1);//Kolor Pól do kupienia
-                g.fillRect((this.getWidth() - BOARD_WIDTH) / 2 + (BOARD_WIDTH - (FIELD_WIDTH + 2)) - tmp, (this.getHeight() - BOARD_HEIGHT) / 2 + (BOARD_HEIGHT - (FIELD_HEIGHT + 2)), FIELD_WIDTH, FIELD_HEIGHT);
-                poz[i][0]=(this.getWidth() - BOARD_WIDTH) / 2 + (BOARD_WIDTH - (FIELD_WIDTH + 2)) - tmp;
-                poz[i][1]=(this.getHeight() - BOARD_HEIGHT) / 2 + (BOARD_HEIGHT - (FIELD_HEIGHT + 2));
-                poz[i][2]=poz[i][0]+FIELD_WIDTH;
-                poz[i][3]=poz[i][1]+FIELD_HEIGHT;
-                tmp += FIELD_WIDTH + 2;
-            }//rysujemy kafelki pomiedzy narożnikami
-            if(start){ setPawnsStart(); start=false;}//ustawia pionki na początku
+        for(int i=0;i<fieldArray.length; i++){
+            fieldArray[i]=new JPanel();
+          //  fieldArray[i].setLayout(new FlowLayout(FlowLayout.CENTER,2,2));
         }
+
+        int x,y;
+        x=100;
+        y=100+FIELD_HEIGHT+FIELD_WIDTH*((fieldnumber/4)-1);
+
+        fieldArray[0].setBounds(x,y, FIELD_HEIGHT, FIELD_HEIGHT);
+        fieldArray[0].setBackground(FIELD_COLOR2);
+
+
+        y-=FIELD_WIDTH;
+        for(int i=1; i<(fieldnumber/4);i++){
+            fieldArray[i].setBounds(x,y, FIELD_HEIGHT, FIELD_WIDTH);;
+            fieldArray[i].setBackground(FIELD_COLOR1);
+            y=y-FIELD_WIDTH;
+        }
+
+        y-=FIELD_WIDTH;
+        fieldArray[(fieldnumber/4)].setBounds(x, y, FIELD_HEIGHT, FIELD_HEIGHT);
+        fieldArray[(fieldnumber/4)].setBackground(FIELD_COLOR2);
+
+        x+=FIELD_HEIGHT;
+        for(int i=(fieldnumber/4)+1; i<2*(fieldnumber/4);i++){
+            fieldArray[i].setBounds(x,y, FIELD_WIDTH, FIELD_HEIGHT);
+            fieldArray[i].setBackground(FIELD_COLOR1);
+            x=x+FIELD_WIDTH;
+        }
+
+        fieldArray[2*(fieldnumber/4)].setBounds(x, y, FIELD_HEIGHT, FIELD_HEIGHT);
+        fieldArray[2*(fieldnumber/4)].setBackground(FIELD_COLOR2);
+
+
+        y+=FIELD_HEIGHT;
+
+        for(int i=2*(fieldnumber/4)+1; i<3*(fieldnumber/4);i++){
+            fieldArray[i].setBounds(x,y, FIELD_HEIGHT, FIELD_WIDTH);
+            fieldArray[i].setBackground(FIELD_COLOR1);
+            y=y+FIELD_WIDTH;
+        }
+
+        fieldArray[3*(fieldnumber/4)].setBounds(x, y, FIELD_HEIGHT, FIELD_HEIGHT);
+        fieldArray[3*(fieldnumber/4)].setBackground(FIELD_COLOR2);
+
+
+        x-=FIELD_WIDTH;
+
+        for(int i=3*(fieldnumber/4)+1; i<fieldnumber;i++){
+            fieldArray[i].setBounds(x,y, FIELD_WIDTH, FIELD_HEIGHT);
+            fieldArray[i].setBackground(FIELD_COLOR1);
+            x=x-FIELD_WIDTH;
+
+        }
+
+        for(int i=0; i<fieldArray.length; i++){
+            /*JLabel index = new JLabel();
+            index.setText("Id: " + i);
+            fieldArray[i].add(index);*/
+            fieldArray[i].setBorder(BorderFactory.createLineBorder(Color.black, 2));
+        }
+
+        rollButton.setBounds(450, 470, ROLL_BUTTON_WIDTH,ROLL_BUTTON_HEIGHT);
+        diceLabel1.setBounds(450, 540, 50, 50);
+        diceLabel2.setBounds(500, 540, 50, 50);
+        for(JPanel m: fieldArray){
+            leftPanel.add(m);
+        }
+
+
     }
+
+    public static javax.swing.JPanel[] getFieldArray() {
+        return fieldArray;
+    }
+
     public void createRollButton()
     {
         rollButton = new JButton("Roll");
+        rollButton.setFocusable(false);
         rollButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -193,8 +192,6 @@ public class GamePanel extends JPanel implements ActionListener {
                 if(round==1) pawn1.placePawnOn(board.getPlayers()[round].getFieldIndex());
                 if(round==2) pawn2.placePawnOn(board.getPlayers()[round].getFieldIndex());
                 if(round==3) pawn3.placePawnOn(board.getPlayers()[round].getFieldIndex());
-
-
                 board.incrementMoveCounter();//na końcu
             }
         });
@@ -202,6 +199,7 @@ public class GamePanel extends JPanel implements ActionListener {
         rollButton.setBackground(Color.WHITE);
         rollButton.setFont(new Font("Serif", Font.BOLD, 28));
     }
+
     public void createDiceLabels() {
         diceLabel1 = new JLabel();
         diceLabel2 = new JLabel();
@@ -210,9 +208,9 @@ public class GamePanel extends JPanel implements ActionListener {
         diceLabel2.setBounds(SCREEN_WIDTH/2, 420, 50, 50);
     }
     public void updateDiceImages(int value1, int value2) {
+        ImageIcon imagePath1 = new ImageIcon(getClass().getResource("/images/dice" + value1 + ".png"));
+        ImageIcon imagePath2 = new ImageIcon(getClass().getResource("/images/dice" + value2 + ".png"));
 
-        ImageIcon imagePath1 = new ImageIcon("src/images/dice" + value1 + ".png");
-        ImageIcon imagePath2 = new ImageIcon("src/images/dice" + value2 + ".png");
 
         Image scaledImage1 = imagePath1.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
         Image scaledImage2 = imagePath2.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
@@ -222,22 +220,11 @@ public class GamePanel extends JPanel implements ActionListener {
 
         diceLabel1.setIcon(icon1);
         diceLabel2.setIcon(icon2);
-
-
     }
-
-
-
-
     @Override
     public void actionPerformed(ActionEvent e) {
-        
-    }
-    public static int[][] getPoz()
-    {
-        return poz;
-    }
 
+    }
 }
 
 
