dc4cf9af5b6469abb908da18dc097efcf5d99f29
TTomaszPWR <280598@student.pwr.edu.pl>
2024-01-13 19:02:40 +0100

    MenuUpdate

--- src/monopolyGame/MenuPanel.java
+++ src/monopolyGame/MenuPanel.java
@@ -33,7 +33,7 @@ public class MenuPanel extends JPanel{
         addTimeLabel();
         this.add(Box.createVerticalStrut(30));
         addTimeSlider();
-        this.add(Box.createVerticalStrut(0));
+        this.add(Box.createVerticalStrut(170));
         addStartButton();
         this.add(Box.createVerticalGlue());
     }
