7c628261d099fcaeb4908e7c6c76bfc4cc6765e1
TTomaszPWR <280598@student.pwr.edu.pl>
2024-01-28 12:30:00 +0100

    Poprawione testy

--- tests/test/ExchangeTest.java
+++ tests/test/ExchangeTest.java
@@ -2,8 +2,8 @@ package test;
 
 import board.Board;
 
-import org.junit.Assert;
 import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
 
 public class ExchangeTest {
 
@@ -14,15 +14,15 @@ public class ExchangeTest {
         Board board = new Board();
         board.setDollarRate(0.8);
         int [] result = new int[]{-100,80};
-        Assert.assertArrayEquals(result, board.ExchangeEURtoUSD(1000, 100, board));
+        assertArrayEquals(result, board.ExchangeEURtoUSD(1000, 100, board));
 
         board.setDollarRate(20);
         int [] result1 = new int[]{-200,4000};
-        Assert.assertArrayEquals(result1, board.ExchangeEURtoUSD(1000, 200, board));
+        assertArrayEquals(result1, board.ExchangeEURtoUSD(1000, 200, board));
 
         board.setDollarRate(0.2);
         int [] result2 = new int[]{-150, 30};
-        Assert.assertArrayEquals(result2, board.ExchangeEURtoUSD(1000, 150, board));
+        assertArrayEquals(result2, board.ExchangeEURtoUSD(1000, 150, board));
 
     }
 
@@ -31,15 +31,15 @@ public class ExchangeTest {
         Board board = new Board();
         board.setEuroRate(0.8);
         int [] result = new int[]{80,-100};
-        Assert.assertArrayEquals(result, board.ExchangeUSDtoEUR(1000, 100, board));
+        assertArrayEquals(result, board.ExchangeUSDtoEUR(1000, 100, board));
 
         board.setEuroRate(20);
         int [] result1 = new int[]{4000,-200};
-        Assert.assertArrayEquals(result1, board.ExchangeUSDtoEUR(1000, 200, board));
+        assertArrayEquals(result1, board.ExchangeUSDtoEUR(1000, 200, board));
 
         board.setEuroRate(0.2);
         int [] result2 = new int[]{30, -150};
-        Assert.assertArrayEquals(result2, board.ExchangeUSDtoEUR(1000, 150, board));
+        assertArrayEquals(result2, board.ExchangeUSDtoEUR(1000, 150, board));
 
     }
 }
